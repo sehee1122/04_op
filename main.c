479938e37882aa9e4b785a9b67ade2b0f24e7934
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2019-09-24 16:08:14 +0900

    04-2_op

--- main.c
+++ main.c
@@ -5,19 +5,28 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x, y, z, m;
+	int x1, y, z, m;
 	int a, b, c;
 	
-	x = 1;
+	x1 = 1;
 	z = 3;
 	a = 3;
 	b = 4;
 	c = 5;
 	
-	y = a*x*x + b*x + c;
-	m = (x + y + z)/3;
+	y = a*x1*x1 + b*x1 + c;
+	m = (x1 + y + z)/3;
 	
-	printf("y=%d, m=%d", y, m);
+	printf("y=%d, m=%d\n\n", y, m);
+	
+	
+	int x2 = 3;
+	int cond1, cond2;
+	
+	cond1 = 2 < x2 && x2 < 5;
+	cond2 = x2 > 1 || x2 < 4 && x2 > 3;
+	
+	printf("cond1=%d,cond2=%d", cond1, cond2);
 	
 	return 0;
 }
